11a57ab520d75c9ce11933fa216f70dda3d11649
Isaiah Gerald <jager4706@yahoo.com>
2021-03-20 02:48:59 -0500

    Update settings.py

--- lonestarbirding/settings.py
+++ lonestarbirding/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#n-s+xbtb$r#um9p+dkh7ahs#8m1vo2t+pbprt48x116_pc$cs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
