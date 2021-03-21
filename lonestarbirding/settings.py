16df1244845aa78f422142732d74b5a594108950
Isaiah Gerald <jager4706@yahoo.com>
2021-03-21 04:03:32 -0500

    Update settings.py

--- lonestarbirding/settings.py
+++ lonestarbirding/settings.py
@@ -126,7 +126,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR,
+##    os.path.join(BASE_DIR, 'static'),
 ##    '/var/www/static/',
 ]
 
